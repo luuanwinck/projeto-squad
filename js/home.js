cd7861b3243eec1fb7de7c0c81ec16469e6e94e3
Luan Winck <113154957+luuanwinck@users.noreply.github.com>
2022-11-15 22:51:27 -0300

    Atualização

---
+++ js/home.js
@@ -0,0 +1,28 @@
+function clickMenu() {
+
+    var a = document.getElementById(itens.style.display)
+
+    if (itens.style.display == 'flex') {
+        itens.style.display = 'none'
+    } else {
+        itens.style.display = 'flex'
+    }
+}
+
+function clickMenu2() {
+    if (login.style.display == 'flex') {
+        login.style.display = 'none'
+    } else {
+        login.style.display = 'flex'
+    }
+}
+
+function mudouTamanho() {
+    if (window.innerWidth >= 992){
+        itens.style.display = 'flex'
+        login.style.display = 'flex'
+    }else{
+        itens.style.display = 'none'
+        login.style.display = 'none'
+    }
+}
